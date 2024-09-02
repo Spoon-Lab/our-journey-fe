7e574e49247c48572e5a088ef5ae94951fb56c41
hijjoy <hijjoy@naver.com>
2024-09-02 18:50:08 +0900

    ✨ Feat: layout 폴더 분리

--- src/components/layouts/default-layout/index.tsx
+++ src/components/layouts/default-layout/index.tsx
@@ -1,4 +1,4 @@
-import Header from '../header';
+import Header from '@/components/header';
 
 import s from './style.module.scss';
 
@@ -6,7 +6,7 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
-export default function GlobalLayout({ children }: LayoutProps) {
+export default function DefaultLayout({ children }: LayoutProps) {
   return (
     <div className={s.layoutContainer}>
       <Header />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app';
 
-import GlobalLayout from '@/components/layouts';
 import ReactQueryProvider from '@/components/providers/ReactQuery';
 
 import '@/styles/globals.scss';
@@ -8,9 +7,7 @@ import '@/styles/globals.scss';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ReactQueryProvider>
-      <GlobalLayout>
-        <Component {...pageProps} />
-      </GlobalLayout>
+      <Component {...pageProps} />
     </ReactQueryProvider>
   );
 }
