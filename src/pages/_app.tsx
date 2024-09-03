e92a41f906c2dcd59c9ce7ec3153db25b95e97fd
hijjoy <hijjoy@naver.com>
2024-09-03 10:58:10 +0900

    ✨ Feat: layout 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,24 @@
+import type { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 
 import ReactQueryProvider from '@/components/providers/ReactQuery';
 
 import '@/styles/globals.scss';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+  return getLayout(
     <ReactQueryProvider>
       <Component {...pageProps} />
-    </ReactQueryProvider>
+    </ReactQueryProvider>,
   );
 }
