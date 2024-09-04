e11b67ef10094f64a313c32b4d1b9559141f1711
hyewon <89793335+hijjoy@users.noreply.github.com>
2024-09-04 13:02:28 +0900

    Revert "[Feat] 공용 Layout 및 Header 컴포넌트 제작 (#9)"
    This reverts commit da15ff8e10a7e17053b745a5d2542171f2cb28af.

--- src/components/providers/ReactQuery.tsx
+++ src/components/providers/ReactQuery.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryStreamedHydration } from '@tanstack/react-query-next-experimental';
 
 export default function ReactQueryProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
@@ -17,7 +18,7 @@ export default function ReactQueryProvider({ children }: { children: React.React
   return (
     <QueryClientProvider client={queryClient}>
       {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}
-      {children}
+      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
     </QueryClientProvider>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,13 @@
-import type { ReactElement, ReactNode } from 'react';
-import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 
 import ReactQueryProvider from '@/components/providers/ReactQuery';
 
 import '@/styles/globals.scss';
 
-export type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
-  return <ReactQueryProvider>{getLayout(<Component {...pageProps} />)}</ReactQueryProvider>;
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ReactQueryProvider>
+      <Component {...pageProps} />
+    </ReactQueryProvider>
+  );
 }
