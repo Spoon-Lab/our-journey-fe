19835ba4759e09d0b2cf811efd0bf8efc7c4c187
hijjoy <hijjoy@naver.com>
2024-09-05 12:03:15 +0900

    ✨ Feat: icons.ts 반영 #3

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,12 @@
-import BELL from '@/assets/icons/Bell.svg';
-
 import s from './style.module.scss';
+
+import { BellIcon } from '@/assets/icons/icons';
 
 export default function Header() {
   return (
     <header className={s.headerContainer}>
       <h1>logo</h1>
-      <BELL />
+      <BellIcon />
     </header>
   );
 }
