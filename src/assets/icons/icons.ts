abeb31c591d154fdb82429547f042726521516b7
drunken_neoguri <developneoguri@gmail.com>
2024-09-13 01:19:27 +0900

    ✨ Feat: 메인 페이지 등에 쓰일 아이콘 추가 #19

---
+++ src/assets/icons/icons.ts
@@ -0,0 +1,6 @@
+import CategoryIcon from './icon-category.svg';
+import HomeIcon from './icon-home.svg';
+import MenuIcon from './icon-menu.svg';
+import SearchIcon from './icon-search.svg';
+
+export { CategoryIcon, HomeIcon, MenuIcon, SearchIcon };
