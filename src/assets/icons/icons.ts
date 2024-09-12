f8ec05e92e89a34557984f9aa70a1c34e6ddca9a
drunken_neoguri <developneoguri@gmail.com>
2024-09-13 01:49:16 +0900

    ✨ Feat: 메인 배너 및 Fab 버튼 위한 아이콘 추가 #19

--- src/assets/icons/icons.ts
+++ src/assets/icons/icons.ts
@@ -1,6 +1,8 @@
+import AddIcon from './icon-add.svg';
 import CategoryIcon from './icon-category.svg';
+import ChevronForwardIcon from './icon-chevron-forward.svg';
 import HomeIcon from './icon-home.svg';
 import MenuIcon from './icon-menu.svg';
 import SearchIcon from './icon-search.svg';
 
-export { CategoryIcon, HomeIcon, MenuIcon, SearchIcon };
+export { AddIcon, CategoryIcon, ChevronForwardIcon, HomeIcon, MenuIcon, SearchIcon };
