7de74302db7a50d88d70dde37501a1984f038fdc
drunken_neoguri <developneoguri@gmail.com>
2024-09-13 22:19:57 +0900

    ✨ Feat: nav bar 버튼 이벤트 및 색상 변경 처리 반영, 임시 경로 추가 #19

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -1,25 +1,41 @@
+import type { MouseEvent } from 'react';
+import { useRouter } from 'next/router';
+
+import { ROUTES } from '@/constants/router';
+
 import s from './style.module.scss';
 
 import { AddIcon, CategoryIcon, HomeIcon, MenuIcon } from '@/assets/icons/icons';
 
 export default function NavBar() {
+  const router = useRouter();
+
+  const { pathname } = router;
+  const mainPath = pathname === ROUTES.main;
+  const categoryPath = pathname === ROUTES.category || pathname === ROUTES.search;
+  const settingPath = pathname === ROUTES.setting;
+
+  const handleMoveToPage = (e: MouseEvent<HTMLButtonElement>) => router.push(e.currentTarget.id);
+
   return (
     <div className={s.navBarContainer}>
-      <button type="button" className={s.navBarButton}>
+      <button id={ROUTES.main} type="button" className={`${s.navBarButton} ${mainPath ? s.currentPage : ''}`} onClick={handleMoveToPage}>
         <HomeIcon />
-        <span className={s.buttonText}>홈</span>
+        <span className={`${s.buttonText} ${mainPath ? s.currentPage : ''}`}>홈</span>
       </button>
-      <button type="button" className={s.navBarButton}>
+      <button id={ROUTES.category} type="button" className={`${s.navBarButton} ${categoryPath ? s.currentPage : ''}`} onClick={handleMoveToPage}>
         <CategoryIcon />
-        <span className={s.buttonText}>카테고리</span>
+        <span className={`${s.buttonText} ${categoryPath ? s.currentPage : ''}`}>카테고리</span>
       </button>
-      <button type="button" className={s.navBarButton}>
+      <button id={ROUTES.setting} type="button" className={`${s.navBarButton} ${settingPath ? s.currentPage : ''}`} onClick={handleMoveToPage}>
         <MenuIcon />
-        <span className={s.buttonText}>메뉴</span>
+        <span className={`${s.buttonText} ${settingPath ? s.currentPage : ''}`}>메뉴</span>
       </button>
-      <button aria-label="글 작성" type="button" className={s.fab}>
-        <AddIcon />
-      </button>
+      {mainPath && (
+        <button aria-label="글 작성" type="button" className={s.fab}>
+          <AddIcon />
+        </button>
+      )}
     </div>
   );
 }
--- src/constants/router.ts
+++ src/constants/router.ts
@@ -1,4 +1,7 @@
 /** 여기서부터 필요한 route 추가해주세요! */
-export const router = {
+export const ROUTES = {
+  category: '/category',
   main: '/main',
+  search: '/search',
+  setting: '/setting',
 };
