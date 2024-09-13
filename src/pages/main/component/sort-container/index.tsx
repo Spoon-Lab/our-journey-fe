0d4af412b3174134404d5c65cda99c7327c86ba3
drunken_neoguri <developneoguri@gmail.com>
2024-09-13 22:15:21 +0900

    ✨ Feat: 페이지 정렬 active, inactive 색상 변경 처리 #27

--- src/pages/main/component/sort-container/index.tsx
+++ src/pages/main/component/sort-container/index.tsx
@@ -10,10 +10,10 @@ interface InterfaceProps {
 export default function SortContainer({ sort, handle }: InterfaceProps) {
   return (
     <div className={s.container}>
-      <button id="resently" type="button" className={s.button} onClick={handle}>
+      <button id="recently" type="button" className={`${s.button} ${sort === 'recently' ? s.active : ''}`} onClick={handle}>
         최신순
       </button>
-      <button id="popularly" type="button" className={s.button} onClick={handle}>
+      <button id="popularly" type="button" className={`${s.button} ${sort === 'popularly' ? s.active : ''}`} onClick={handle}>
         인기순
       </button>
     </div>
