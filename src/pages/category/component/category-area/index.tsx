bad5c1201065b6adcad53939ac31e7be7a7f5c6b
drunken_neoguri <developneoguri@gmail.com>
2024-09-14 20:46:13 +0900

    ✨ Feat: 카테고리 버튼을 Button -> Link로 교체 #27

--- src/pages/category/component/category-area/index.tsx
+++ src/pages/category/component/category-area/index.tsx
@@ -1,23 +1,20 @@
-import type { MouseEvent } from 'react';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
+
+import { ROUTES } from '@/constants/router';
 
 import s from './style.module.scss';
 
 export default function CategoryArea() {
-  const router = useRouter();
-
-  const handleMovePage = (e: MouseEvent<HTMLButtonElement>) => router.push(`/search?categoryId=${e.currentTarget.id}`);
-
   return (
-    <div style={{ width: '100%', padding: '0 16px' }}>
+    <div className={s.categoryListWrapper}>
       <h3 className={s.categoryLabel}>카테고리</h3>
       <div className={s.categoryBox}>
-        <button type="button" className={s.categoryButton} id="domestic" name="국내 여행" onClick={handleMovePage}>
+        <Link type="button" className={s.categoryButton} href={`${ROUTES.search}?id=domestic`}>
           국내 여행
-        </button>
-        <button type="button" className={s.categoryButton} id="foreign" name="해외 여행" onClick={handleMovePage}>
+        </Link>
+        <Link type="button" className={s.categoryButton} href={`${ROUTES.search}?id=foreign`}>
           해외 여행
-        </button>
+        </Link>
       </div>
     </div>
   );
