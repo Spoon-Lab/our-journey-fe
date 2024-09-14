2a1ccdf60d46e22976b4aad878cf03b7ddba475c
drunken_neoguri <developneoguri@gmail.com>
2024-09-14 14:56:45 +0900

    ✨ Feat: 하단 내비게이션 바 관련 Button -> Link로 변경 #19

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -1,4 +1,5 @@
 import type { MouseEvent } from 'react';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 import { ROUTES } from '@/constants/router';
@@ -15,26 +16,24 @@ export default function NavBar() {
   const categoryPath = pathname === ROUTES.category || pathname === ROUTES.search;
   const settingPath = pathname === ROUTES.setting;
 
-  const handleMoveToPage = (e: MouseEvent<HTMLButtonElement>) => router.push(e.currentTarget.id);
-
   return (
     <div className={s.navBarContainer}>
-      <button id={ROUTES.main} type="button" className={`${s.navBarButton} ${mainPath ? s.currentPage : ''}`} onClick={handleMoveToPage}>
+      <Link id={ROUTES.main} type="button" className={`${s.navBarButton} ${mainPath ? s.currentPage : ''}`} href={ROUTES.main}>
         <HomeIcon />
         <span className={`${s.buttonText} ${mainPath ? s.currentPage : ''}`}>홈</span>
-      </button>
-      <button id={ROUTES.category} type="button" className={`${s.navBarButton} ${categoryPath ? s.currentPage : ''}`} onClick={handleMoveToPage}>
+      </Link>
+      <Link id={ROUTES.category} type="button" className={`${s.navBarButton} ${categoryPath ? s.currentPage : ''}`} href={ROUTES.category}>
         <CategoryIcon />
         <span className={`${s.buttonText} ${categoryPath ? s.currentPage : ''}`}>카테고리</span>
-      </button>
-      <button id={ROUTES.setting} type="button" className={`${s.navBarButton} ${settingPath ? s.currentPage : ''}`} onClick={handleMoveToPage}>
+      </Link>
+      <Link id={ROUTES.setting} type="button" className={`${s.navBarButton} ${settingPath ? s.currentPage : ''}`} href={ROUTES.setting}>
         <MenuIcon />
         <span className={`${s.buttonText} ${settingPath ? s.currentPage : ''}`}>메뉴</span>
-      </button>
+      </Link>
       {mainPath && (
-        <button id={ROUTES.create} aria-label="글 작성" type="button" className={s.fab} onClick={handleMoveToPage}>
+        <Link id={ROUTES.create} aria-label="글 작성" type="button" className={s.fab} href={ROUTES.create}>
           <AddIcon />
-        </button>
+        </Link>
       )}
     </div>
   );
