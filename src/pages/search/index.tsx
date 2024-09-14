99863c1a95787bd09c1ec52901fa65b55765141f
drunken_neoguri <developneoguri@gmail.com>
2024-09-14 22:55:39 +0900

    ✨ Feat: 무한 스크롤 반영 위한 hook 추가 및 검색 페이지에 반영 #27

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,7 +1,8 @@
-import type { ReactNode } from 'react';
+import { MouseEvent, type ReactNode, useRef } from 'react';
 import { useRouter } from 'next/router';
 
 import useGetFeed from '@/hooks/use-get-feed';
+import { useIntersectionObserver } from '@/hooks/use-intersection-observer';
 
 import FeedGrid from '@/components/feed-grid';
 import Header from '@/components/header';
@@ -14,23 +15,29 @@ export default function Search() {
   const router = useRouter();
   const { title, categoryId } = router.query;
 
-  const { data } = useGetFeed({ title: title as string | undefined, categoryId: categoryId as string | undefined });
+  const { data, fetchNextPage, hasNextPage } = useGetFeed({ title: title as string | undefined, categoryId: categoryId as string | undefined });
 
   const categoryName = (categoryId && (categoryId === 'domestic' ? '국내여행' : '해외여행')) ?? undefined;
 
-  if (!data) {
-    return <div />;
-  }
+  const divRef = useRef<HTMLDivElement>(null);
+
+  useIntersectionObserver({
+    ref: divRef,
+    onIntersect: (entry) => {
+      if (entry.isIntersecting && hasNextPage) {
+        void fetchNextPage();
+      }
+    },
+  });
 
   return (
     <>
-      <Header title={`${categoryName ?? (title as string) ?? ''} 검색결과 ${data.pages[0].totalElements}건`} />
+      <Header title={`${categoryName ?? (title as string) ?? ''} 검색결과 ${data?.pages[0].totalElements}건`} />
       <section className={s.searchWrapper}>
-        {data.pages.map((content) => (
-          <FeedGrid key={content.pageable.pageNumber} data={content.content} />
-        ))}
-        <NavBar />
+        {data?.pages.map((content) => <FeedGrid key={content.pageable.pageNumber} data={content.content} />)}
+        <div className={s.refArea} ref={divRef} />
       </section>
+      <NavBar />
     </>
   );
 }
