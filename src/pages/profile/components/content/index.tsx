c73b5ab44afc0a92cda8d66c2e7cb7ce759d44b0
hijjoy <hijjoy@naver.com>
2024-09-19 21:43:17 +0900

    ✨ Feat: 빌드오류 수정
    - 구조분해 -> 옵셔널 체이닝으로 수정

--- src/pages/profile/components/content/index.tsx
+++ src/pages/profile/components/content/index.tsx
@@ -6,15 +6,14 @@ import s from './style.module.scss';
 // TODO:spring에서 주는 데이터에 작성글 내용이 없어서 변경해야할것같음
 
 export default function ContentItem({ content }: { content: Content }) {
-  const { title, nickname, postImg } = content;
   return (
     <div className={s.container}>
-      <Image src={postImg} alt="content img" width={66} height={66} />
+      <Image src={content?.postImg} alt="content img" width={66} height={66} />
       <div className={s.contentWrapper}>
-        <h1>{title}</h1>
+        <h1>{content?.title}</h1>
         <div className={s.contentBox}>
           <p className={s.content}>작성글 내용</p>
-          <p className={s.nickname}>{nickname}</p>
+          <p className={s.nickname}>{content?.nickname}</p>
         </div>
       </div>
     </div>
