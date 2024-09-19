5fef82e00065e9c0991d95b2adf418d27da096e4
drunken_neoguri <developneoguri@gmail.com>
2024-09-19 20:55:38 +0900

    ✨ Feat: 코드 리뷰 사항 반영하여 너비에 따라 자동으로 줄임 처리되도록 수정 #27

--- src/components/grid-card/index.tsx
+++ src/components/grid-card/index.tsx
@@ -12,15 +12,13 @@ export default function GridCard({ data }: { data: Content }) {
     return <div />;
   }
 
-  const title = data.title.length > 15 ? `${data.title.substring(0, 15)}...` : data.title;
-
   return (
     <Link className={s.cardWrapper} href={`${ROUTES.content}/${data.contentId}`}>
       <figure className={s.thumbnailBox}>
         <img alt={data.title} src={data.postImg} className={s.thumbnail} />
         <span className={s.date}>{formatDate(new Date(data.createdAt))}</span>
       </figure>
-      <h3 className={s.cardTitle}>{title}</h3>
+      <h3 className={s.cardTitle}>{data.title}</h3>
       <div className={s.profileBox}>
         <img className={s.profileThumbnail} src={data.contentProfileDto.profileImgUrl} alt={`${data.contentProfileDto.name} 님의 사진`} />
         <span className={s.profileName}>{data.contentProfileDto.name}</span>
