0a355f4bc7a32f05506f7ab590d182ba6e84660f
drunken_neoguri <developneoguri@gmail.com>
2024-09-21 00:11:25 +0900

    ✨ Feat: 현 response 값 기준으로 Grid Card 내 데이터 반영 #33

--- src/components/grid-card/index.tsx
+++ src/components/grid-card/index.tsx
@@ -13,14 +13,14 @@ export default function GridCard({ data }: { data: Content }) {
   }
 
   return (
-    <Link className={s.cardWrapper} href={`${ROUTES.content}/${data.contentId}`}>
+    <Link className={s.cardWrapper} href={`${ROUTES.detail}?id=${data.contentId}`}>
       <figure className={s.thumbnailBox}>
-        <img alt={data.title} src={data.postImg} className={s.thumbnail} />
+        <img alt={data.title} src={data.postImg} className={s.thumbnail} loading="lazy" />
         <span className={s.date}>{formatDate(new Date(data.createdAt))}</span>
       </figure>
       <h3 className={s.cardTitle}>{data.title}</h3>
       <div className={s.profileBox}>
-        <img className={s.profileThumbnail} src={data.contentProfileDto.profileImgUrl} alt={`${data.contentProfileDto.name} 님의 사진`} />
+        <img className={s.profileThumbnail} src={data.contentProfileDto.profileImgUrl} alt={`${data.contentProfileDto.name} 님의 사진`} loading="lazy" />
         <span className={s.profileName}>{data.contentProfileDto.name}</span>
       </div>
     </Link>
