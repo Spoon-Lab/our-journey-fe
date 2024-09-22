4809de97ced9583d5775039228fd304b07b7b478
drunken_neoguri <developneoguri@gmail.com>
2024-09-22 15:54:48 +0900

    ✨ Feat: 임시 비로그인 상태의 axios 추가 (차후 통합 필요) #35

--- src/pages/category/component/category-area/index.tsx
+++ src/pages/category/component/category-area/index.tsx
@@ -9,11 +9,11 @@ export default function CategoryArea() {
     <div className={s.categoryListWrapper}>
       <h3 className={s.categoryLabel}>카테고리</h3>
       <div className={s.categoryBox}>
-        <Link type="button" className={`${s.categoryButton} ${s.domestic}`} href={`${ROUTES.search}?categoryId=domestic`}>
-          국내 여행
+        <Link type="button" className={`${s.categoryButton} ${s.domestic}`} href={`${ROUTES.search}?categoryId=1`}>
+          <span>국내 여행</span>
         </Link>
-        <Link type="button" className={`${s.categoryButton} ${s.foreign}`} href={`${ROUTES.search}?categoryId=foreign`}>
-          해외 여행
+        <Link type="button" className={`${s.categoryButton} ${s.foreign}`} href={`${ROUTES.search}?categoryId=2`}>
+          <span>해외 여행</span>
         </Link>
       </div>
     </div>
