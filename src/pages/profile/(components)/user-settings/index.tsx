3d486198b3db56652b68131724887aa4ca1ae48e
hyewon <89793335+hijjoy@users.noreply.github.com>
2024-09-27 00:05:08 +0900

    [Feat] 로그아웃 api 연결 & 리프레시 수정 (#57)
    * ✨ Feat: 로그아웃 api 수정 #56
    
    * ✨ Feat: 리프레시 토큰 로직 수정 #56

--- src/pages/profile/(components)/user-settings/index.tsx
+++ src/pages/profile/(components)/user-settings/index.tsx
@@ -5,12 +5,12 @@ import s from './style.module.scss';
 function UserSettings() {
   // TODO: 버튼 액션
 
-  const { mutate } = useLogout();
+  const { mutate: logout } = useLogout();
 
   return (
     <div className={s.settingContainer}>
       <button type="button">고객센터</button>
-      <button type="button" onClick={() => mutate()}>
+      <button type="button" onClick={() => logout()}>
         로그아웃
       </button>
       <button type="button">회원탈퇴</button>
