2972cf1d034da3a7fd3e40294f09afaa7a9700d4
hijjoy <hijjoy@naver.com>
2024-09-27 02:41:45 +0900

    ✨ Feat: 로그아웃 api #66

--- src/pages/profile/(components)/user-settings/index.tsx
+++ src/pages/profile/(components)/user-settings/index.tsx
@@ -1,23 +1,16 @@
-import { useRouter } from 'next/router';
-
-import { ROUTES } from '@/constants/router';
+import useLogout from '@/hooks/auth/use-logout';
 
 import s from './style.module.scss';
 
 function UserSettings() {
   // TODO: 버튼 액션
-  const router = useRouter();
 
-  // const { mutate: logout } = useLogout();
-  const logout = () => {
-    localStorage.clear();
-    void router.push(ROUTES.base);
-  };
+  const { mutate: logout } = useLogout();
 
   return (
     <div className={s.settingContainer}>
       <button type="button">고객센터</button>
-      <button type="button" onClick={logout}>
+      <button type="button" onClick={() => logout()}>
         로그아웃
       </button>
       <button type="button">회원탈퇴</button>
