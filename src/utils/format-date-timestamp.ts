e9591bdab0a93be52230ff3d791df044b6608052
HyeGyeong Kim <51890074+i-nooo@users.noreply.github.com>
2024-09-27 22:46:13 +0900

    🐛 Bug: fix build err

--- src/utils/format-date-timestamp.ts
+++ src/utils/format-date-timestamp.ts
@@ -1,10 +1,13 @@
 export const formatTimeStamp = (date: string): string => {
+  if (!date) return '';
   const [datePart] = date.split(' ');
   const [year, month, day] = datePart.split('-');
   return `${year} ${month}.${day}`;
 };
 
 export const defaultFormatTimeStamp = (date: string): string => {
+  if (!date) return '';
+
   const [datePart] = date.split(' ');
   const [year, month, day] = datePart.split('-');
   return `${year}.${month}.${day}`;
