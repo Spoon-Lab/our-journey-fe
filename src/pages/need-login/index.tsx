9102019c1b1c930656cae2b3982537fbce8cb688
drunken_neoguri <developneoguri@gmail.com>
2024-09-29 20:01:52 +0900

    ✨ feat: 비로그인 상태의 리다이렉트 화면에서, 로그인 화면으로 넘어갈 수 있도록 수정 #50

--- src/pages/need-login/index.tsx
+++ src/pages/need-login/index.tsx
@@ -15,10 +15,16 @@ export default function Login() {
         <hr />
         <h1>{`해당 기능을 이용하려면\n회원 가입이 필요해요!!`}</h1>
         <p>{`회원가입하고 아워저니를\n더 신나게 즐겨보세요!`}</p>
+        <p>{`이미 서비스에 가입하셨다면,\n로그인 화면으로 돌아가 로그인을 진행해주세요!`}</p>
       </div>
-      <Link href={ROUTES.signup} className={s.moveLinkSignUp}>
-        <span>회원가입</span>
-      </Link>
+      <div className={s.moveLinkBox}>
+        <Link href={ROUTES.login} className={s.moveLink}>
+          <span>로그인</span>
+        </Link>
+        <Link href={ROUTES.signup} className={s.moveLink}>
+          <span>회원가입</span>
+        </Link>
+      </div>
     </main>
   );
 }
