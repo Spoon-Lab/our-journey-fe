054dbf6018d4d2e049677f533f9df1a04d30758d
drunken_neoguri <developneoguri@gmail.com>
2024-10-04 22:22:34 +0900

    ✨ Feat: 설문조사 이벤트 배너 배치 #127

--- src/mocks/banners.ts
+++ src/mocks/banners.ts
@@ -1,20 +1,27 @@
 export const Banners = [
   {
-    bannerId: '57',
+    bannerId: '1',
+    chipText: [],
+    contentId: '0',
+    src: '',
+    title: '',
+  },
+  {
+    bannerId: '2',
     chipText: ['오사카', '외곽_지역', '힐링 여행'],
     contentId: '57',
     src: 'https://our-journey-fe.vercel.app/_next/image?url=https%3A%2F%2Fspoon-ourjourney.s3.ap-northeast-2.amazonaws.com%2Fmedia%2Fcontent%2F20240506_191132.jpg&w=2048&q=75',
     title: '오사카에는 도톤보리와 신사이바시,\n난바만 있는 게 아니에요.',
   },
   {
-    bannerId: '56',
+    bannerId: '3',
     chipText: ['성수동', '핫플레이스', '지역 탐방'],
     contentId: '56',
     src: 'https://our-journey-fe.vercel.app/_next/image?url=https%3A%2F%2Fspoon-ourjourney.s3.ap-northeast-2.amazonaws.com%2Fmedia%2Fcontent%2F1000022520.jpg&w=2048&q=75',
     title: 'MZ 세대 핫플레이스!\n성수동에서 느낄 수 있는 감성플레이스',
   },
   {
-    bannerId: '58',
+    bannerId: '4',
     chipText: ['태국', '워케이션', '코워킹스페이스'],
     contentId: '58',
     src: 'https://our-journey-fe.vercel.app/_next/image?url=https%3A%2F%2Fspoon-ourjourney.s3.ap-northeast-2.amazonaws.com%2Fmedia%2Fcontent%2F20230607_203632.jpg&w=2048&q=75',
