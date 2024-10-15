4f9dac83fec940013adbf1094df9e1abfcccf7ed
HyeGyeong Kim <51890074+i-nooo@users.noreply.github.com>
2024-10-16 00:11:12 +0900

    🗑️ Remove: 안 쓰는 hook 삭제

--- src/hooks/use-get-route-param-number.ts
+++
@@ -1,8 +0,0 @@
-import { useParams } from 'next/navigation';
-
-type ParamKey = string;
-
-export function useGetRouteParamNumber<T extends ParamKey = 'id'>(paramKey: T = 'id' as T): number {
-  const params = useParams()?.[paramKey];
-  return params !== undefined ? Number(params) : -1;
-}
